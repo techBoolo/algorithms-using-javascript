7c0f9537e7eddebe2439118823e5eb514a0501c2
teklu <teklufa@gmail.com>
2023-03-19 16:18:37 +0300

    confirmBeginning

---
+++ tests/confirmBeginning.test.js
@@ -0,0 +1,7 @@
+import confirmBeginning from '../algorithms/confirmBeginning.js'
+
+test('the input must start with the given target', () => {
+  let inputData = 'Bearer jsonwebtoken' 
+  let target = 'bearer'
+  expect(confirmBeginning(inputData, target)).toBeTruthy()
+})
